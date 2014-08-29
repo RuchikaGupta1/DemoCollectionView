f41624504087ec62d42bfab6e6de0d127388da9b
Ruchika <ruchika@IK11s-iMac.local>
2014-08-29 10:08:09 +0530

    Initial Commit

---
+++ DemoCollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoCollectionView
+//
+//  Created by Ruchika on 29/08/14.
+//  Copyright (c) 2014 Ruchika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
